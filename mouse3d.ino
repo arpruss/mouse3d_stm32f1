16f9992c14ea248b4560f168a9bbf96445c10333
arpruss <arpruss@gmail.com>
2018-09-23 09:18:09 -0500

    protection

--- mouse3d.ino
+++ mouse3d.ino
@@ -2,17 +2,23 @@
 // Needs USBComposite library: 
 #include <USBComposite.h>
 
-//#define MOUSE_ON_SERIAL1
+#define MOUSE_ON_SERIAL1
+#define DEBUG
+#define POWER_CONTROL PB11
 
 #ifdef MOUSE_ON_SERIAL1
 #define SER Serial1
 #else
 #define SER USBCompositeSerial
+#ifdef DEBUG
+# error Debug needs MOUSE_ON_SERIAL1
+#endif
 #endif
 
-#undef JOYSTICK_MODE
+//#define JOYSTICK_MODE
 #define LED PB12
 
+bool joyMode = false;
 static const int16 trimValue = 500;
 static const uint8 minLow = (-trimValue)&0xFF;
 static const uint8 minHigh = ((-trimValue)>>8)&0xFF;
@@ -33,11 +39,7 @@ int16 trim(int16 v) {
 
 uint8_t descriptor_mouse3d[] = {
   0x05, 0x01,           /*  Usage Page (Generic Desktop) */ 
-#ifdef JOYSTICK_MODE
-  0x09, 0x04,           /*  0x08: Usage (Joystick) */ 
-#else  
   0x09, 0x08,           /*  0x08: Usage (Multi-Axis Controller) */ 
-#endif  
   0xA1, 0x01,           /*  Collection (Application) */ 
   0xa1, 0x00,            // Collection (Physical)
   0x85, 0x01,         /*  Report ID */
@@ -79,12 +81,82 @@ uint8_t descriptor_mouse3d[] = {
   0x81, 0x02,           /*    Input (variable,absolute) */ 
   0xC0,
 
+  0xa1, 0x00,           // Collection (Physical)
+  0x85, 0x04,           //  Report ID 
+  0x05, 0x08,       /*   USAGE_PAGE (LEDs) */ 
+  0x19, 0x01,       /*   USAGE_MINIMUM (1) */ 
+  0x29, 0x08,       /*   USAGE_MAXIMUM (8)*/ 
+  0x95, 0x08,       /*   REPORT_COUNT (8) */ 
+  0x75, 0x01,       /*   REPORT_SIZE (1) */ 
+  0x91, 0x02,         /*   OUTPUT (Data,Var,Abs) */    
+  0xC0,
+
+  0xC0
+};
+
+uint8_t descriptor_joy3d[] = {
+  0x05, 0x01,           /*  Usage Page (Generic Desktop) */ 
+  0x09, 0x04,           /*  0x08: Usage (Joystick) */ 
+  0xA1, 0x01,           /*  Collection (Application) */ 
+  0xa1, 0x00,            // Collection (Physical)
+  0x85, 0x01,         /*  Report ID */
+  0x16, minLow,minHigh,        //logical minimum (-500)
+  0x26, maxLow,maxHigh,        //logical maximum (500)
+  0x36,0x00,0x80,              // Physical Minimum (-32768)
+  0x46,0xff,0x7f,              //Physical Maximum (32767)
+  0x09, 0x30,           /*    Usage (X) */ 
+  0x09, 0x31,           /*    Usage (Y) */ 
+  0x09, 0x32,           /*    Usage (Z) */ 
+  0x75, 0x10,           /*    Report Size (16) */ 
+  0x95, 0x03,           /*    Report Count (3) */ 
+  0x81, 0x02,           /*    Input (variable,absolute) */ 
+  0xC0,                           /*  End Collection */ 
+
+  0xa1, 0x00,            // Collection (Physical)
+  0x85, 0x02,         /*  Report ID */
+  0x16, minLow,minHigh,        //logical minimum (-500)
+  0x26, maxLow,maxHigh,        //logical maximum (500)
+  0x36, 0x00,0x80,              // Physical Minimum (-32768)
+  0x46, 0xff,0x7f,              //Physical Maximum (32767)
+  0x09, 0x33,           /*    Usage (RX) */ 
+  0x09, 0x34,           /*    Usage (RY) */ 
+  0x09, 0x35,           /*    Usage (RZ) */ 
+  0x75, 0x10,           /*    Report Size (16) */ 
+  0x95, 0x03,           /*    Report Count (3) */ 
+  0x81, 0x02,           /*    Input (variable,absolute) */ 
+  0xC0,                           /*  End Collection */ 
+    
+  0xa1, 0x00,            // Collection (Physical)
+  0x85, 0x03,         /*  Report ID */
+  0x15, 0x00,           /*   Logical Minimum (0) */ 
+  0x25, 0x01,           /*    Logical Maximum (1) */
+  0x75, 0x01,           /*    Report Size (1) */ 
+  0x95, 24,           /*    Report Count (24) */
+  0x05, 0x09,           /*    Usage Page (Button) */ 
+  0x19, 1,           /*    Usage Minimum (Button #1) */ 
+  0x29, 24,           /*    Usage Maximum (Button #24) */ 
+  0x81, 0x02,           /*    Input (variable,absolute) */ 
+  0xC0,
+
+  0xa1, 0x00,           // Collection (Physical)
+  0x85, 0x04,           //  Report ID 
+  0x05, 0x08,       /*   USAGE_PAGE (LEDs) */ 
+  0x19, 0x01,       /*   USAGE_MINIMUM (1) */ 
+  0x29, 0x08,       /*   USAGE_MAXIMUM (8)*/ 
+  0x95, 0x08,       /*   REPORT_COUNT (8) */ 
+  0x75, 0x01,       /*   REPORT_SIZE (1) */ 
+  0x91, 0x02,         /*   OUTPUT (Data,Var,Abs) */    
+  0xC0,
+
   0xC0
 };
 
 USBHID HID;
 #ifndef MOUSE_ON_SERIAL1
 USBCompositeSerial SER;
+#endif
+#ifdef DEBUG
+USBCompositeSerial CompositeSerial;
 #endif
 
 typedef struct {
@@ -116,10 +188,13 @@ public:
     HIDReporter xyzReporter;
     HIDReporter rxyzReporter;
     HIDReporter buttonsReporter;
+    HIDBuffer_t ledData;
+    uint8_t leds[HID_BUFFER_ALLOCATE_SIZE(1,1)];
     HIDMouse3D(USBHID& HID) 
             : xyzReporter(HID, (uint8_t*)&xyz, sizeof(xyz), 1),
               rxyzReporter(HID, (uint8_t*)&rxyz, sizeof(rxyz), 2),
-              buttonsReporter(HID, (uint8_t*)&buttons, sizeof(buttons), 3) 
+              buttonsReporter(HID, (uint8_t*)&buttons, sizeof(buttons), 3),
+              ledData(leds, HID_BUFFER_SIZE(1,4), 4, HID_BUFFER_MODE_NO_WAIT)
               {}
     void sendPosition() {
       xyzReporter.sendReport();
@@ -129,42 +204,92 @@ public:
     void sendButtons() {
       buttonsReporter.sendReport();
     }
+
+    void begin() {
+      HID.addOutputBuffer(&ledData);      
+    }
 };
 
 HIDMouse3D Mouse3D(HID);
 
-void setup() {
-  pinMode(LED, OUTPUT);
-
-#ifdef JOYSTICK_MODE
-  USBComposite.setManufacturerString("stm32duino");
-  USBComposite.setProductString("Mouse3D");
-  USBComposite.setVendorId(0x1EAF);
-#else
+void startMouse3D() {  
+  USBComposite.clear();
   USBComposite.setManufacturerString("3dconnexion"); // "stm32duino");
   USBComposite.setProductString("SpaceMouse Pro"); // "Mouse3D");
   USBComposite.setVendorId(0x46D);
-#endif
   USBComposite.setProductId(0xc62b); 
+  HID.clearBuffers();
   HID.setReportDescriptor(descriptor_mouse3d, sizeof(descriptor_mouse3d));
   HID.registerComponent();
-#ifndef MOUSE_ON_SERIAL1  
+  Mouse3D.begin();
+#ifndef MOUSE_ON_SERIAL1
   SER.registerComponent();
-#endif  
+#endif
+#ifdef DEBUG
+  CompositeSerial.registerComponent();
+#endif
   USBComposite.begin();
-  delay(1000);
-  SER.begin(9600);
+}
 
-  digitalWrite(LED,0);
+void startJoy3D() {  
+  USBComposite.clear();
+  USBComposite.setManufacturerString("stm32duino");
+  USBComposite.setProductString("Mouse3D");
+  USBComposite.setVendorId(0x1EAF);
+  USBComposite.setProductId(0xc62b); 
+  HID.clearBuffers();
+  HID.setReportDescriptor(descriptor_joy3d, sizeof(descriptor_joy3d));
+  HID.registerComponent();
+#ifndef MOUSE_ON_SERIAL1
+  SER.registerComponent();
+#endif
+#ifdef DEBUG
+  CompositeSerial.registerComponent();
+#endif
+  USBComposite.begin();
+}
+
+void setup() {
+#ifdef MOUSE_ON_SERIAL1
+  pinMode(POWER_CONTROL, OUTPUT);
+  digitalWrite(POWER_CONTROL, 1);
+  pinMode(PA9, OUTPUT);
+  digitalWrite(PA9, 1);
+#endif
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, 0);
+
+  if (joyMode) 
+    startJoy3D();
+  else
+    startMouse3D();
+  delay(200);
+  
+  digitalWrite(LED,1);
+#ifdef MOUSE_ON_SERIAL1
+  digitalWrite(POWER_CONTROL, 0);
+#endif  
+  delay(300);
+  SER.begin(9600,SERIAL_8N2);
+  delay(500);
+
   uint32 t = millis();
   while((millis()-t) < 2000) {
-    if (SER.available()) Serial.read();
+    if (SER.available()) {
+      uint8_t c = Serial.read();
+#ifdef DEBUG
+      CompositeSerial.write(c);      
+#endif      
+    }
   }
   SER.write(preface);  
-  digitalWrite(LED,1);
+  while((millis()-t) < 2000) {
+    if (SER.available()) Serial.read();
+  } 
+  SER.write(preface);  
   Mouse3D.sendButtons();
-  Mouse3D.sendPosition();
-  
+  Mouse3D.sendPosition();  
 }
 
 #define BUFFER_SIZE 128
@@ -185,6 +310,33 @@ void processBuffer(const uint8* buf, uint32 len) {
       //haveHandedness = true;
       b = ((b&0b111111) | ((b&~0b1111111)>>1)) & 0b111111111111;
       b = ((b >> 9) | (b << 3)) & 0b111111111111;
+#ifdef DEBUG
+      CompositeSerial.println(String(b));
+#endif      
+#ifdef SWITCHABLE_BROKEN
+      if ((b & (0b111 << 6)) == (0b111 << 6)) {
+        if (joyMode && (b & (1 << 3))) {
+          joyMode = false;
+          HID.end();
+          USBComposite.end();
+          SER.write("S\r");
+          delay(1000);
+          startMouse3D();
+          delay(1000);
+          return;
+        }
+        else if (!joyMode && (b & (1 << 4))) {
+          joyMode = true;
+          HID.end();
+          USBComposite.end();
+          SER.write("S\r");
+          delay(1000);
+          startJoy3D();
+          delay(1000);
+          return;
+        }
+      }
+#endif      
       Mouse3D.buttons.buttons = b;
       Mouse3D.sendButtons();
     }
@@ -213,14 +365,16 @@ void processBuffer(const uint8* buf, uint32 len) {
 }
 
 void loop() {
-  if (millis()-lastD > 5000) {
-    SER.write(preface);  
-    lastD = millis();
+  if (millis() >= lastD + 5000) {
+     SER.write("M\r");
+     lastD = millis();
   }
+//  digitalWrite(LED,(Mouse3D.leds[1] & 0x3) == 0);
   while (SER.available()) {
-    digitalWrite(LED,0);  
     uint8 c = SER.read();
-    digitalWrite(LED,1);
+#ifdef DEBUG
+    CompositeSerial.write(c);
+#endif        
     if (c == '\r') {
       if (bufferPos < BUFFER_SIZE) 
         processBuffer(buffer,bufferPos);
